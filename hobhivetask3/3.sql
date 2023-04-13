b146b1a22e111c756b4181a6c153df38c2ffd855
shimanskijvl <hob2023026@mipt-client.atp-fivt.org>
2023-04-13 19:59:17 +0000

    Fix 1

--- hobhivetask3/3.sql
+++ hobhivetask3/3.sql
@@ -9,12 +9,12 @@ SET hive.exec.reducers.max=8;
 USE shimanskijvl;
 
 WITH t1 AS (
-    select content.userInn as inn, from_unixtime(content.dateTime.datee, 'dd') as daay, sum(content.totalSum) as sum from kkt where subtype = 'receipt' group by content.userInn, 2 order by sum 
+    select content.userInn as inn, from_unixtime(content.dateTime.datee, 'dd') as daay, NVL(sum(content.totalSum), 0 ) as sum from kkt group by content.userInn, 2 order by sum 
 )
 SELECT r.inn, r.daay, r.sum
 FROM   (SELECT e.*, 
-               DENSE_RANK() OVER 
-                 (PARTITION BY e.daay ORDER BY e.sum DESC) AS pos
+               RANK() OVER 
+                 (PARTITION BY e.inn ORDER BY e.sum DESC) AS pos
         FROM   t1 AS e) AS r
-WHERE  r.pos <= 3
-ORDER BY r.inn DESC;
+WHERE  r.pos = 1
+ORDER BY r.inn;
