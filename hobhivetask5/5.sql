f2fddda3f42a375551e1842c9017a755bbe72bb5
shimanskijvl <hob2023026@mipt-client.atp-fivt.org>
2023-04-14 21:02:45 +0000

    Fifth task

---
+++ hobhivetask5/5.sql
@@ -0,0 +1,13 @@
+add jar /opt/cloudera/parcels/CDH/lib/hive/lib/json-serde-1.3.8-jar-with-dependencies.jar;
+
+SET hive.cli.print.header=false;
+SET mapred.input.dir.recursive=true;
+SET hive.mapred.supports.subdirectories=true;
+SET hive.groupby.orderby.position.alias=true;
+SET mapreduce.job.reduces=8;
+
+USE shimanskijvl_test;
+
+with tbl as (
+    select content.userInn as inn, subtype, LAG(subtype, 1, '') OVER (ORDER BY 1, content.dateTime.datee) as prevtype, LEAD(subtype, 1, '') OVER (ORDER BY 1, content.dateTime.datee) as nexttype from kkt
+) select tbl.inn from tbl where tbl.subtype = 'receipt' and ( tbl.prevtype = 'closeShift' or tbl.nexttype = 'openShift' ) group by tbl.inn order by tbl.inn limit 50;
