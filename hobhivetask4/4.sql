e3324d134f7fc9490cafe1b0b187b38cb8fa4016
shimanskijvl <hob2023026@mipt-client.atp-fivt.org>
2023-04-14 20:48:48 +0000

    Ascending order

--- hobhivetask4/4.sql
+++ hobhivetask4/4.sql
@@ -10,4 +10,4 @@ USE shimanskijvl_test;
 with sum as (
   select content.userInn as inn, from_unixtime(content.dateTime.datee, 'HH') < 13 as daay, round(avg(content.totalSum), 0) as sum from kkt where subtype = 'receipt' group by content.userInn, 2
 )
-select left_sum.inn, left_sum.sum, right_sum.sum from sum left_sum join sum right_sum on (left_sum.inn = right_sum.inn ) where left_sum.daay and not right_sum.daay and (left_sum.sum > right_sum.sum) order by left_sum.sum desc;
+select left_sum.inn, left_sum.sum, right_sum.sum from sum left_sum join sum right_sum on (left_sum.inn = right_sum.inn ) where left_sum.daay and not right_sum.daay and (left_sum.sum > right_sum.sum) order by left_sum.sum limit 50;
